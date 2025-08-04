dce0f6a723f4ac40e6d6dc2d3990508ddba3f279
dhruvKhambhata <khambhatadhruv28@gmail.com>
2025-08-04 22:41:25 +0530

    feature : added vercel Speed Insights

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import { Inter } from "next/font/google"
 import "./globals.css"
 import { Toaster } from "@/components/ui/toaster"
 import { ThemeProvider } from "@/components/theme-provider"
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 const inter = Inter({ subsets: ["latin"] })
 
@@ -52,6 +53,7 @@ export default function RootLayout({
         <ThemeProvider attribute="class" defaultTheme="system" enableSystem disableTransitionOnChange>
           {children}
           <Toaster />
+          <SpeedInsights/>
         </ThemeProvider>
       </body>
     </html>
