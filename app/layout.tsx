42b182a33621c1b7b25f5841126ea00b9c8a5d2e
dhruvKhambhata <khambhatadhruv28@gmail.com>
2025-08-04 22:43:52 +0530

    changes : added analytics for vercel

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,7 @@ import "./globals.css"
 import { Toaster } from "@/components/ui/toaster"
 import { ThemeProvider } from "@/components/theme-provider"
 import { SpeedInsights } from "@vercel/speed-insights/next"
+import { Analytics } from "@vercel/analytics/next"
 
 const inter = Inter({ subsets: ["latin"] })
 
@@ -54,6 +55,7 @@ export default function RootLayout({
           {children}
           <Toaster />
           <SpeedInsights/>
+          <Analytics/>
         </ThemeProvider>
       </body>
     </html>
